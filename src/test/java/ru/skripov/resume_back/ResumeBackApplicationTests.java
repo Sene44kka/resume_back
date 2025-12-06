8d0a8c4e36aad2318c9768aa704b4734504d1433
skripov <skripov96@mail.ru>
2025-12-06 21:54:37 +0500

    create new project

---
+++ src/test/java/ru/skripov/resume_back/ResumeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skripov.resume_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
